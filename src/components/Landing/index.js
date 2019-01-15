9297fcb5f28fd4bcb7bbe57266610ab439b9faac
root <root@LAPTOP-PF4KMCBR.localdomain>
2019-01-15 23:19:57 +0000

    POC authentification

---
+++ src/components/Landing/index.js
@@ -0,0 +1,14 @@
+
+import './Landing.css';
+import logo from './logo.svg';
+import React from 'react';
+
+const Landing = () => (
+  <div>
+    <h1>Landing</h1>
+	<img src={logo} className="App-logo" alt="logo" />
+  </div>
+  
+);
+
+export default Landing;
